a739203c81a4b32abbc3afb9fa979abead95c645
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2020-11-16 22:17:44 +0000

    Add settings fragment

---
+++ app/src/main/java/com/example/android/navigationsamples/SettingsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.android.navigationsamples
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<Toolbar>(R.id.settings_toolbar)
+        NavigationUI.setupWithNavController(toolbar, findNavController())
+    }
+
+}
